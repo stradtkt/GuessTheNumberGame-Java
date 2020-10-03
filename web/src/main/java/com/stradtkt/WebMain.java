7d6830f40e372df88e26d9839cc9ebba872033db
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-03 12:43:52 -0400

    Added the home view

---
+++ web/src/main/java/com/stradtkt/WebMain.java
@@ -0,0 +1,12 @@
+package com.stradtkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class WebMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMain.class, args);
+    }
+}
---
+++ web/src/main/java/com/stradtkt/util/ViewNames.java
@@ -0,0 +1,6 @@
+package com.stradtkt.util;
+
+public final class ViewNames {
+    public static final String HOME = "home";
+    private ViewNames() {}
+}
