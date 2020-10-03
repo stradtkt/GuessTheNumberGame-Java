afa236ce0239d7e346bf5e1dfe3f7f888f22f849
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-03 13:13:30 -0400

    Created a game service

---
+++ web/src/main/java/com/stradtkt/service/GameServiceImpl.java
@@ -0,0 +1,51 @@
+package com.stradtkt.service;
+
+import com.stradtkt.Game;
+import com.stradtkt.MessageGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
+public class GameServiceImpl implements GameService {
+    private final Game game;
+    private final MessageGenerator messageGenerator;
+    @Autowired
+    public GameServiceImpl(Game game, MessageGenerator messageGenerator) {
+        this.game = game;
+        this.messageGenerator = messageGenerator;
+    }
+    @PostConstruct
+    public void init() {
+        log.info("number = {}", game.getNumber());
+        log.info("mainMessage = {}", messageGenerator.getMainMessage());
+    }
+    @Override
+    public boolean isGameOver() {
+        return game.isGameWon() || game.isGameLost();
+    }
+
+    @Override
+    public String getMainMessage() {
+        return messageGenerator.getMainMessage();
+    }
+
+    @Override
+    public String getResultMessage() {
+        return messageGenerator.getResultMessage();
+    }
+
+    @Override
+    public void checkGuess(int guess) {
+        game.setGuess(guess);
+        game.check();
+    }
+
+    @Override
+    public void reset() {
+        game.reset();
+    }
+}
